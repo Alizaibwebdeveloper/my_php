33a1295ed619285c0043404b14e9047cfbba1726
Ali zaib <alizaib12220@gmail.com>
2024-03-01 15:39:57 +0500

    Calculator code practice in php

--- hassan_Bai.php
+++ hassan_Bai.php
@@ -1,6 +1,7 @@
 <?php
-
+//Without Class Properties
 class Calculator {
+    //add function to calculate sum of two numbers
     public function add($a, $b) {
         return $a + $b;
     }
@@ -17,10 +18,32 @@ $result_subtraction = $calculator->subtract(10, 4);
 echo "Result of addition: $result_addition .'<br>'";
 echo "Result of subtraction: $result_subtraction.'<br>'";
 
+//With Class Properties
+class Calculator {
+    public $a;
+    public $b;
+    function __construct__($a, $b) {
+        if ($a<0 || $b<0){
+            throw new Exception('Parameters cannot be lesser then zero.');
+        }
+        $this->a = $a;
+        $this->b = $b;
+    
+    }
+    public function add() {
+        return $this->a + $this->b;
+    }
 
-
-
-echo "<h1>Here  Below the calculator Using procedural Programming</h1>";
+    public function subtract() {
+        return $this->a -  $this->b;
+    }
+}
+$calculator1 = new Calculator(1,2);
+$result_addition1 = $calculator1->add(); 
+$result_subtraction1 = $calculator1->subtract();
+echo "Result of addition: $result_addition1 .'<br>'";
+echo "Result of subtraction: $result_subtraction1.'<br>'";
+echo "<h1>Here  Below the calculator Using proceral Programming</h1>";
 
 
 
@@ -44,9 +67,6 @@ function calculator($operation, $operand1, $operand2) {
 }
 
 
-
-
-
 $result_addition = calculator('add', 5, 3);
 $result_subtraction = calculator('subtract', 10, 4);
 
@@ -54,5 +74,3 @@ echo "Result of addition: $result_addition <br>";
 echo "Result of subtraction: $result_subtraction";
 
 ?>
-
-
